060742889222120af0e1e05e02cec1067d17dfab
Riyane El Qoqui <riyane.dev@gmail.com>
2025-11-01 17:24:07 +0100

    feat(config): implement TOML parser for LoadConfig

---
+++ internal/config/parser.go
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 Riyane El Qoqui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("error during reading file %s: %w", path, err)
+	}
+
+	var cfg Config
+	if err := toml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("error during parsing toml of file %s: %w", path, err)
+	}
+
+	return &cfg, nil
+}
